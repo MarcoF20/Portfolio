9f60cff0a9c02575f4f0cf004aaa5b672ecbd8c0
Marco Franco <piratafranco20@gmail.com>
2023-08-08 23:10:56 -0700

    Initial commit
    Create project, add dependencies, create navbar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import linkedinIcon from '../assets/linkedin-icon.svg'
+import githubIcon from '../assets/github-icon.svg'
+
+export function NavigationBar () {
+  return (
+    <>
+      <Navbar expand='lg'>
+        <Container id='navContainer'>
+          <Navbar.Brand href='#home'>franco.dev</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='https://www.linkedin.com/in/marco-franco-4b2b88218/' target='_blank'>
+                <img src={linkedinIcon} alt='LinkedIn Logo' className='navIcon' />
+              </Nav.Link>
+              <Nav.Link href='https://github.com/MarcoF20' target='_blank'>
+                <img src={githubIcon} alt='LinkedIn Logo' className='navIcon' />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container />
+    </>
+  )
+}
