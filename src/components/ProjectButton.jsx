5562eb79ad59ba84637bff45059a808ff958b8e3
Marco Franco <piratafranco20@gmail.com>
2023-08-20 22:11:13 -0700

    Add new component ProjectButton
    The button in the project cards it can also be a dropdown button if there is a live demo for the project

---
+++ src/components/ProjectButton.jsx
@@ -0,0 +1,23 @@
+import Button from 'react-bootstrap/Button'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Dropdown from 'react-bootstrap/Dropdown'
+
+export function ProjectButton ({ demo, repo }) {
+  if (demo !== false) {
+    return (
+      <Dropdown as={ButtonGroup}>
+        <Button as='a' className='projectButton' href={repo} target='_blank' rel='noreferrer'>Repository</Button>
+
+        <Dropdown.Toggle split variant='primary' id='dropdown-split-basic' className='toggleDropdown' />
+
+        <Dropdown.Menu>
+          <Dropdown.Item href='#/action-1'>Live demo</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    )
+  } else {
+    return (
+      <Button as='a' href={repo} className='projectButton' target='_blank' rel='noreferrer'>Repository</Button>
+    )
+  }
+}
